4cf2e1c713170612ddd82bb044a0eb77cabb411c
Steven <stevenpmcfarlane@gmail.com>
2021-03-29 22:28:57 -0600

    fixed some tables

--- api/diners/diner-model.js
+++ api/diners/diner-model.js
@@ -3,16 +3,20 @@
 // build your `Project` model here
 // db = require('../data/db-config')
 
-// const findDiner = () => {
-//     return db('diners as d')
-//     .select('d.*')
-// }
+//get for diner
 
 // //get array of favorite Trucks by id
-// const findById = (truck_id) => {
-//     return db('trucks as t')
+// const findFavoriteTrucks = (diner_id) => {
+//    need to track down fav trucks according to diner_id
+//    go from diners to fav_trucks to trucks; need a join for diners, trucks, and fav_trucks
 // }
+//select.*
+//diners d
+//join fav_trucks f on f.diner_id = d.diner_id
+//join trucks t on t.truck_id = f.truck_id
+//where
+//d.diner_id = diner_id
 
 // module.exports = {
-//     findDiner, findById
-// } 
+//     findById, findFavoriteTrucks
+// }
