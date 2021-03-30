4044630728a2c866832dd2604d10bbc381f1dd83
Liza Pincsak <lizapincsak@MacBook-Air-2.lan>
2021-03-29 19:18:40 -0500

    table working

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,15 +1,174 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('user_username', 200).notNullable()
-      users.string('user_password', 200).notNullable()
-      users.string('user_email', 320).notNullable()
-      users.integer('role').notNullable()
-      users.timestamps(false, true)
+    .createTable("users", (users) => {
+      users.increments("user_id");
+      users.string("user_username", 200).notNullable();
+      users.string("user_password", 200).notNullable();
+      users.string("user_email", 320).notNullable();
+      users.integer("role").notNullable();
+      users.timestamps(false, true);
     })
-}
+    .createTable("trucks", (tbl) => {
+      tbl.increments("truck_id");
+      tbl.binary("truck_img").notNullable();
+      tbl.string("cuisine_type", 28).notNullable();
+      tbl.time("departure_time").notNullable();
+      // tbl
+      //   .integer("truck_location_id")
+      //   .unsigned()
+      //   .notNullable()
+      //   .references("truck_location_id")
+      //   .inTable("truck_locations")
+      //   .onDelete("RESTRICT")
+      //   .onUpdate("RESTRICT");
+    })
+    .createTable("truck_locations", (tbl) => {
+      tbl.increments("truck_location_id");
+      //tbl.something("truck_coordinates");//gps coordinates
+      tbl
+        .integer("truck_id")
+        .unsigned()
+        .notNullable()
+        .references("truck_id")
+        .inTable("trucks")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    })
+    .createTable("diners", (tbl) => {
+      tbl.increments("diner_id");
+      tbl.integer("role_id").notNullable();
+      //tbl.text("diner_location"); need gps
+      // tbl.integer("fav_truck_id");
+      tbl
+        .integer("fav_truck_id")
+        .unsigned()
+        .notNullable()
+        .references("truck_id")
+        .inTable("trucks")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    })
+    .createTable("operators", (tbl) => {
+      tbl.increments("operator_id");
+      tbl.integer("role_id", 128).notNullable();
+    })
+    .createTable("menuitems", (tbl) => {
+      tbl.increments("menuitem_id");
+      tbl.string("item_name", 28).notNullable();
+      tbl.text("item_description").notNullable();
+      tbl.binary("item_img").notNullable();
+      tbl.float("item_price").notNullable();
+    })
+    .createTable("trucks_menuitems", (tbl) => {
+      tbl.increments("trucks_menuitems_id");
+      tbl
+        .integer("truck_id")
+        .unsigned()
+        .notNullable()
+        .references("truck_id")
+        .inTable("trucks")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      tbl
+        .integer("menuitem_id")
+        .unsigned()
+        .notNullable()
+        .references("menuitem_id")
+        .inTable("menuitems")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    })
+    .createTable("operator_trucks", (tbl) => {
+      tbl.increments("operator_trucks_id");
+      tbl
+        .integer("operator_id")
+        .unsigned()
+        .notNullable()
+        .references("operator_id")
+        .inTable("operators")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      tbl
+        .integer("truck_id")
+        .unsigned()
+        .notNullable()
+        .references("truck_id")
+        .inTable("trucks")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    })
+    .createTable("ratings", (tbl) => {
+      tbl.increments("rating_id");
+      tbl.integer("rating")
+      tbl.float("avg_cust_rating").notNullable;
+      tbl
+        .integer("truck_id")
+        .unsigned()
+        .notNullable()
+        .references("truck_id")
+        .inTable("trucks")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      tbl
+        .integer("menuitem_id")
+        .unsigned()
+        .notNullable()
+        .references("menuitem_id")
+        .inTable("menuitems")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    })
+    .createTable("diner_favetruck", (tbl) => {
+      tbl.increments("diner_favetruck_id");
+      tbl
+        .integer("diner_id")
+        .unsigned()
+        .notNullable()
+        .references("diner_id")
+        .inTable("diners")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      tbl
+        .integer("truck_id")
+        .unsigned()
+        .notNullable()
+        .references("truck_id")
+        .inTable("trucks")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    })
+    .createTable("diner_truck_locations", (tbl) => {
+      tbl.increments("diner_truck_location_id");
+      tbl
+        .integer("truck_location_id")
+        .unsigned()
+        .notNullable()
+        .references("truck_id")
+        .inTable("trucks")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      tbl
+        .integer("diner_id")
+        .unsigned()
+        .notNullable()
+        .references("diner_id")
+        .inTable("diners")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    });
+};
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
-}
+  await knex.schema.dropTableIfExists("diner_truck_locations")
+  await knex.schema.dropTableIfExists("diner_favtruck")
+  await knex.schema.dropTableIfExists("ratings")
+  await knex.schema.dropTableIfExists("operator_trucks")
+  await knex.schema.dropTableIfExists("trucks_menuitems")
+  await knex.schema.dropTableIfExists("menuitems")
+  await knex.schema.dropTableIfExists("diners")
+  await knex.schema.dropTableIfExists("operators")
+ 
+  await knex.schema.dropTableIfExists("truck_locations")
+  await knex.schema.dropTableIfExists("trucks")
+  await knex.schema.dropTableIfExists("users")
+} 
